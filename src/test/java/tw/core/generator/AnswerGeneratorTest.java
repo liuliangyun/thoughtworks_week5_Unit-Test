a7925230a0fe56da06c4713ed8deda322907ed11
unknown <you@example.com>
2018-04-20 14:34:54 +0800

    The Unit Test for AnswerGenerator -- AnswerGeneratorTest.java is running successfully!

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,43 @@
 package tw.core.generator;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
 
+    private RandomIntGenerator randomIntGenerator;
+    private AnswerGenerator answerGenerator;
+
+    @Before
+    public void setUp() throws Exception {
+        randomIntGenerator = mock(RandomIntGenerator.class);
+        answerGenerator = new AnswerGenerator(randomIntGenerator);
+    }
+
+    @Test
+    public void should_generate_an_answer() throws OutOfRangeAnswerException {
+        // Given
+        int digitalmax = 9;
+        int numbersOfNeed = 4;
+        String inputStr = "1 2 3 4";
+        Answer answer = null;
+
+        // When
+        when(randomIntGenerator.generateNums(digitalmax, numbersOfNeed)).thenReturn(inputStr);
+        answer = answerGenerator.generate();
+
+        // Then
+        assertNotNull(answer);
+    }
 }
 
