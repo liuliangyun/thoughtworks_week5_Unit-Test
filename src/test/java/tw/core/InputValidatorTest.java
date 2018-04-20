c995d8720a5725d64da5cf422970d5a12b506025
unknown <you@example.com>
2018-04-20 13:28:19 +0800

    The Unit Test for InputValidator -- InputValidatorTest.java is running successfully!

--- src/test/java/tw/core/InputValidatorTest.java
+++ src/test/java/tw/core/InputValidatorTest.java
@@ -1,8 +1,47 @@
 package tw.core;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.validator.InputValidator;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * 在InputValidatorTest文件中完成InputValidator中对应的单元测试
  */
 public class InputValidatorTest {
+    private InputValidator inputValidator;
 
+    @Before
+    public void setUp() throws Exception {
+        inputValidator = new InputValidator();
+    }
+
+    @Test
+    public void should_get_false_when_numstr_integer_is_repeat() {
+        // Given
+        String numstr = "1 1 2 3";
+
+        // Then
+        assertFalse(inputValidator.validate(numstr));
+    }
+
+    @Test
+    public void should_get_false_when_numstr_integer_length_is_not_4() {
+        // Given
+        String numstr = "1 2 3";
+
+        // Then
+        assertFalse(inputValidator.validate(numstr));
+    }
+
+    @Test
+    public void should_get_true_when_numstr_is_validate() {
+        // Given
+        String numstr = "1 2 3 4";
+
+        // Then
+        assertTrue(inputValidator.validate(numstr));
+    }
 }
