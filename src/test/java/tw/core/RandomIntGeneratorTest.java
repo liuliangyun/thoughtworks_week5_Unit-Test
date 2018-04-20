974895e63a2353c4bcc42b1d13cd89302c0c2484
unknown <you@example.com>
2018-04-20 13:01:28 +0800

    The Unit Test for RandomIntGenerator -- RandomIntGeneratorTest.java is running successfully!

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,29 @@
 package tw.core;
 
 
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
+    @Test
+    public void should_get_string_length__is_4_when_numbersOfNeed_is_4() {
+        // Given
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
 
+        // When
+        String result = randomIntGenerator.generateNums(9, 4);
+        List<String> resultList = Arrays.stream(result.split(" ")).collect(Collectors.toList());
+
+        // Then
+        assertEquals(4, resultList.size());
+    }
 }
